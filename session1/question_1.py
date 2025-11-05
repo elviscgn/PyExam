899d0547f60e3d7bc8c026d357213c05bd74b1a8
Elvis <96030189+elviscgn@users.noreply.github.com>
2025-11-05 16:31:34 +0200

    Implement fizzbuzz function skeleton
    Define fizzbuzz function with docstring for implementation.

---
+++ session1/question_1.py
@@ -0,0 +1,20 @@
+# ==============================
+# Question 1 - FizzBuzz (10) marks
+# ==============================
+
+def fizzbuzz(n):
+    """
+    Returns a list of strings from 1 to n:
+    - "Fizz" if divisible by 3
+    - "Buzz" if divisible by 5
+    - "FizzBuzz" if divisible by both
+    - otherwise the number as a string
+
+    Parameters:
+    n (int): the maximum number
+
+    Returns:
+    list[str]: list of strings representing FizzBuzz output
+    """
+    # TODO: implement function
+    pass
